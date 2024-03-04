63305568df61937e2a9cc3b5d90e4b0863c6d4aa
satyam809 <54880971+satyam809@users.noreply.github.com>
2024-03-04 23:34:13 +0530

    Update transportationController.js

--- controllers/transportationController.js
+++ controllers/transportationController.js
@@ -20,7 +20,7 @@ exports.updateTransportation = async (req, res) => {
             return res.status(404).json({ status: false,message: 'Transportation not found' });
         }
 
-        await transportation.save(); // This will only update fields that have changed
+        await transportation.update({ ...req.body });
 
         res.status(200).json({ status: true, data: transportation, message: 'Transportation updated successfully' });
     } catch (error) {
