0da9180879bd5791aefafbcfc53da7f4e0ee2a23
satyam809 <54880971+satyam809@users.noreply.github.com>
2024-03-11 17:49:53 +0530

    update database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,12 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
+// const sequelize = new Sequelize('new_crm', 'root', '', {
+//     port: 3308,
+//     host: 'localhost',
+//     dialect: 'mysql', // or any other dialect
+//     timezone: '+05:30'
+// });
 const sequelize = new Sequelize('tripzfp3_app_crm_db', 'tripzfp3_app_crm_db', '@Tripzygo@2023#', {
     //port: 3308,
     host: '103.211.218.27',
