34a6d26beec6f81267bf81730d2730cb7409cb4e
satyam809 <54880971+satyam809@users.noreply.github.com>
2024-03-19 18:09:01 +0530

    new commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3002;
+require('dotenv').config();
 const db = require('./database');
 // app.use("/", (req, res) => {
 //     console.log('test');
--- database.js
+++ database.js
@@ -1,19 +1,3 @@
-// db.js
-// const mongoose = require('mongoose');
-
-// const uri = 'mongodb://localhost:27017/tripzygoCrm'; // MongoDB connection URI
-
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-//   });
-
-// module.exports = mongoose.connection;
-
-require('dotenv').config(); // Load environment variables from .env file
 
 const mongoose = require('mongoose');
 
