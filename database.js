5321eca965275518512560b0eaaf2a6add993957
satyam809 <54880971+satyam809@users.noreply.github.com>
2024-03-19 15:48:21 +0530

    use live mongodb

--- database.js
+++ database.js
@@ -1,7 +1,23 @@
 // db.js
+// const mongoose = require('mongoose');
+
+// const uri = 'mongodb://localhost:27017/tripzygoCrm'; // MongoDB connection URI
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
+
+// module.exports = mongoose.connection;
+
+require('dotenv').config(); // Load environment variables from .env file
+
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/tripzygoCrm'; // MongoDB connection URI
+const uri = process.env.MONGODB_URI; // MongoDB connection URI loaded from environment variable
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -12,3 +28,5 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   });
 
 module.exports = mongoose.connection;
+
+
